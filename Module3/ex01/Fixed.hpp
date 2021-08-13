62556ec9b7aeb053083075bc4bddad0d3ed6a043
Evgenii <geniya59@gmail.com>
2021-08-13 16:20:27 +0300

    add

---
+++ Module3/ex01/Fixed.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <iostream>
+
+class Fixed
+{
+private:
+    int _fixedValue;
+    static const int _numFractionalBits;
+public:
+    Fixed(/* args */);
+    Fixed( const int value );
+    Fixed( const float value );
+    Fixed(const Fixed &other);
+    float toFloat( void ) const;
+    int toInt( void ) const;
+    Fixed & operator = (const Fixed &other);
+    int getRawBits( void ) const;
+    void setRawBits( int const raw );
+    ~Fixed();
+};
+
+std::ostream & operator << (std::ostream &o, Fixed const &other);
